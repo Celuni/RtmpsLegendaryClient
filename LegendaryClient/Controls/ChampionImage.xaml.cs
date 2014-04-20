abfffc8a75cb4f13eddde235723f0ecd5da05243
eddy5641 <your_email@example.com>
2014-04-20 12:38:07 -0700

    First Push

---
+++ LegendaryClient/Controls/ChampionImage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LegendaryClient.Controls
+{
+    /// <summary>
+    /// Interaction logic for ChampionImage.xaml
+    /// </summary>
+    public partial class ChampionImage : UserControl
+    {
+        public ChampionImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LegendaryClient/Controls/ProfileChampionImage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LegendaryClient.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProfileChampionImage.xaml
+    /// </summary>
+    public partial class ProfileChampionImage : UserControl
+    {
+        public ProfileChampionImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
